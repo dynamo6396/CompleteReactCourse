2ba3c646ffee446a617d1f9b706f0132647d9bd7
dynamo6396 <jatingoyal6396@gmail.com>
2024-04-23 16:36:41 +0530

    Complex nesting of tags

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ Now to create nested element through react
     <div class="child">
         <h1> This is h1 tag </h1>
     </div>
+    <div class="child">
+        <h1> This is h1 tag </h1>
+    </div>
 </div>
  */
-const parent =React.createElement("div",{class:"parent"},React.createElement("div",{class:"child"},React.createElement("h1",{},"Hey I am heading ")));
+
+const parent =React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"Hey I am heading "),React.createElement("h2",{},"Hey I am a h2 tag ")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"Hey I am heading "),React.createElement("h2",{},"Hey I am a h2 tag ")])]);
+// this is looking complex but it is made easy by jsx. 
+
+console.log(parent);
+
 root.render(parent);
