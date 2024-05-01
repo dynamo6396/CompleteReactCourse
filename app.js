064ff967e1990e30df1074bb1afe3578570167ef
dynamo6396 <jatingoyal6396@gmail.com>
2024-05-01 15:31:52 +0530

    Episode3 end

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { jsx } from "react/jsx-runtime";
 
 //  React Element 
 const jsxheading =(<h1 id="heading"> Namaste React using JSX 🎈</h1> );
-const Title=()=> (
+const Title = (
      <h2> Namaste React </h2>
 );
 {/* This is called component composition  */}
-const f2 =function(){
-    return (<div>Hello world 2 </div>)
-}
-const f1 =()=>{
-    return (<h1>Hello world </h1>)
-}
 const Functionalcomponent =()=>(
     <div>
         <Title/>
